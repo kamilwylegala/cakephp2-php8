1567742bc93fe9146dbc899cb5f0446644fc595a
Koji Tanaka <tenkoma@gmail.com>
2023-01-07 21:12:58 +0900

    test: Fix CakeEmailTest::testResetWithCharset()
    Co-authored-by: Kenshin Okinaka <okinakak@yahoo.co.jp>

--- lib/Cake/Test/Case/Network/Email/CakeEmailTest.php
+++ lib/Cake/Test/Case/Network/Email/CakeEmailTest.php
@@ -1893,8 +1893,8 @@ class CakeEmailTest extends CakeTestCase {
 		$this->CakeEmail->charset = 'ISO-2022-JP';
 		$this->CakeEmail->reset();
 
-		$this->assertSame('utf-8', $this->CakeEmail->charset, $this->CakeEmail->charset);
-		$this->assertNull($this->CakeEmail->headerCharset, $this->CakeEmail->headerCharset);
+		$this->assertSame('utf-8', $this->CakeEmail->charset);
+		$this->assertNull($this->CakeEmail->headerCharset);
 	}
 
 /**
