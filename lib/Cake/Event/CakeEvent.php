de9504196932b11392e0a1231bfa96c0b38f4e82
Kamil Wylegala <kamil.wylegala@gmail.com>
2023-06-02 23:18:08 +0200

    Fixed PHP 8.2 deprecation notices in CakeEvent class. (#43)
    * Fixed PHP 8.2 deprecation notices in CakeEvent class.

--- lib/Cake/Event/CakeEvent.php
+++ lib/Cake/Event/CakeEvent.php
@@ -24,6 +24,30 @@
 class CakeEvent {
 
 /**
+ * PHP 8.2 deprecation notice: added to avoid `Creation of dynamic property ... is deprecated.`
+ * @var mixed|true
+ */
+	public mixed $break;
+
+/**
+ * PHP 8.2 deprecation notice: added to avoid `Creation of dynamic property ... is deprecated.`
+ * @var mixed|true
+ */
+	public mixed $modParams;
+
+/**
+ * PHP 8.2 deprecation notice: added to avoid `Creation of dynamic property ... is deprecated.`
+ * @var array|mixed
+ */
+	public mixed $breakOn;
+
+/**
+ * PHP 8.2 deprecation notice: added to avoid `Creation of dynamic property ... is deprecated.`
+ * @var array|mixed
+ */
+	public mixed $omitSubject;
+
+/**
  * Name of the event
  *
  * @var string
