f9f5e79560b3a982462ae3ee4a4746f0393b3f99
Markus Bauer <markus.bauer@cispa.saarland>
2024-07-24 19:22:18 +0200

    Fix SecurityComponent typing

--- lib/Cake/Controller/Component/SecurityComponent.php
+++ lib/Cake/Controller/Component/SecurityComponent.php
@@ -344,7 +344,7 @@ class SecurityComponent extends Component {
  * @link https://book.cakephp.org/2.0/en/core-libraries/components/security-component.html#handling-blackhole-callbacks
  * @throws BadRequestException
  */
-	public function blackHole(?Controller $controller, $error = '', ?SecurityException $exception = null) {
+	public function blackHole(Controller $controller, $error = '', ?SecurityException $exception = null) {
 		if (!$this->blackHoleCallback) {
 			$this->_throwException($exception);
 		}
@@ -822,7 +822,7 @@ class SecurityComponent extends Component {
  * @param string $method Method to execute
  * @param array $params Parameters to send to method
  * @return mixed Controller callback method's response
- * @throws BadRequestException When a the blackholeCallback is not callable.
+ * @throws BadRequestException When the blackholeCallback is not callable.
  */
 	protected function _callback(Controller $controller, $method, $params = array()) {
 		if (!is_callable(array($controller, $method))) {
